88ecb1300a3c9c21c247246f1cd2e568e8d815ce
Samuel <samuelamorim865@gmail.com>
2019-10-26 17:57:43 -0300

    Initial commit

---
+++ app/src/main/java/br/com/eetepaoriximina/expoeetepa2019/activities/CadastroActivity.java
@@ -0,0 +1,16 @@
+package br.com.eetepaoriximina.expoeetepa2019.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.eetepaoriximina.expoeetepa2019.R;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
---
+++ app/src/main/java/br/com/eetepaoriximina/expoeetepa2019/activities/LoginActivity.java
@@ -0,0 +1,19 @@
+package br.com.eetepaoriximina.expoeetepa2019.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.eetepaoriximina.expoeetepa2019.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+
+
+    }
+}
